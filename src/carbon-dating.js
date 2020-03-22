027bd9705fa3ab7d19fc28212b60177acafbc73a
Dmitry Rukovichnikov <dmitry.rukovichnikov@gmail.com>
2020-03-22 23:49:13 +0300

    complete task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,21 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof Number(sampleActivity) !== "number"){
+    return false;
+  }
+
+  if(!sampleActivity || sampleActivity <= 0 || sampleActivity > 15 || typeof sampleActivity !== "string"){
+    return false
+  }
+  sampleActivity = Number(sampleActivity);
+  
+   console.log (sampleActivity); 
+ let k = 0.693/HALF_LIFE_PERIOD;
+ let t = (Math.log(MODERN_ACTIVITY / sampleActivity))/k;
+
+t = Math.floor(t) + 1;
+return t
+ 
 };
